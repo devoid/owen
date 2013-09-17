8c8721cf6a568e2dfa61ae55c561bab8859c4c0e
Scott Devoid <devoid@anl.gov>
2013-09-17 11:54:10 -0500

    Move database and cli to separate modules

--- owen/cmd/ibm_ticket.py
+++ owen/cmd/ibm_ticket.py
@@ -11,89 +11,14 @@ import sqlalchemy.sql
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime
 from sqlalchemy.orm import relationship, backref, sessionmaker
 
-
-import owen.cli as cli
-
-DEFAULT_DB_PATH = '/root/service_tickets.sqlite'
-
-def warn(error):
-    print >>sys.stderr, error
-
-def die(error):
-    warn(error)
-    sys.exit(1)
-
-
-
-
-## Objects ##
-Base = declarative_base()
-class Machine(Base):
-    __tablename__ = 'machines'
-    
-    id = Column(Integer, primary_key=True)
-    hostname = Column(String)
-    machine_type = Column(String)
-    serial = Column(String)
-    
-    def __repr__(self):
-        return "<Machine('%s','%s','%s')>" % (
-            self.hostname, self.machine_type, self.serial)
-
-class Part(Base):
-    __tablename__ = 'parts'
-    
-    id = Column(Integer, primary_key=True)
-    fru = Column(String)
-    description = Column(String) 
-
-    def __repr__(self):
-        return "<Part('%s','%s')>" % (self.description, self.fru)
-    
-
-
-class PartRequest(Base):
-    __tablename__ = 'part_requests'
-
-    id = Column(Integer, primary_key=True)
-    closed = Column(Boolean, default=False)
-    machine_id = Column(Integer, ForeignKey('machines.id'))
-    machine = relationship("Machine",
-        backref=backref('part_requests', order_by=id))
-    part_id = Column(Integer, ForeignKey('parts.id'))
-    part = relationship("Part",
-        backref=backref('requested', order_by=id))
-    part_count = Column(Integer)
-    ticket_number = Column(String)
-    status = Column(String)
-    date_created = Column(DateTime,
-        default=datetime.now(),
-        server_default=sqlalchemy.sql.func.current_timestamp())
-    date_closed  = Column(DateTime)
-    
-    def close(self):
-        self.closed = True
-        self.date_closed = datetime.now()
-
-def get_model_and_serial(host):
-    filename = os.path.join(ASU_COLLECTION_DIR, host)
-    if not os.path.isfile(filename):
-        filename = os.path.join(ASU_COLLECTION_DIR, host + '.conf')
-    if not os.path.isfile(filename):
-        return ('Unknown','Unknown')
-    # Model
-    cmd = [ 'grep', 'SYSTEM_PROD_DATA.SysInfoProdName', filename ]
-    _, model = subprocess.check_output(cmd).rstrip().split('=')
-    # Serial
-    cmd = [ 'grep', 'SYSTEM_PROD_DATA.SysInfoSerialNum', filename ]
-    _, serial = subprocess.check_output(cmd).rstrip().split('=')
-    return (model, serial)
+from owen import cli
+from owen.db import models as models
 
 class TicketShell(cli.Shell):
     def __init__(self, database):
         database = os.path.abspath(database)
         self.engine = sqlalchemy.create_engine('sqlite:///'+database) 
-        Base.metadata.create_all(self.engine)
+        models.Base.metadata.create_all(self.engine)
         session_class = sessionmaker(bind=self.engine)
         self.session = session_class()
 
@@ -119,9 +44,9 @@ class TicketShell(cli.Shell):
     @cli.arg('--closed', action='store_true', help='Include closed tickets')
     @cli.arg('--csv', action='store_true', help='Print as CSV')
     def do_ticket_list(self, args):
-        q = self.session.query(PartRequest)
+        q = self.session.query(models.PartRequest)
         if not args.closed:
-            q = q.filter(PartRequest.closed != True)
+            q = q.filter(models.PartRequest.closed != True)
         columns = [ 'status', 'id', 'hostname', 'model', 'serial', 'part',
                     'fru', 'count', 'ticket_number', 'created on',
                     'closed', 'closed on']
@@ -133,7 +58,7 @@ class TicketShell(cli.Shell):
                 obj.part.description, obj.part.fru, obj.part_count,
                 obj.ticket_number, obj.date_created, obj.closed,
                 obj.date_closed ]))
-        q.order_by(PartRequest.date_created)
+        q.order_by(models.PartRequest.date_created)
         self._table(q.all(), columns, formatter=row_format, as_csv=args.csv)
 
     @cli.arg('--hostname', required=True, help="Machine hostname")
@@ -141,31 +66,31 @@ class TicketShell(cli.Shell):
     @cli.arg('--status', help="Optional current status message")
     @cli.arg('--count', help="Number of items requested", default=1)
     def do_ticket_create(self, args):
-        machine = self.session.query(Machine).\
-            filter(Machine.hostname == args.hostname).first()
-        part = self.session.query(Part).\
-            filter(Part.id == args.part).first()
+        machine = self.session.query(models.Machine).\
+            filter(models.Machine.hostname == args.hostname).first()
+        part = self.session.query(models.Part).\
+            filter(models.Part.id == args.part).first()
         if machine and part:
-            self._add_obj(PartRequest(
+            self._add_obj(models.PartRequest(
                 status=args.status, machine=machine,
                 part=part, part_count=args.count))
         elif not machine:
-            die("Unknown machine hostname %s" % args.hostname)
+            cli.die("Unknown machine hostname %s" % args.hostname)
         elif not part:
-            die("Unknown part id %s" % args.part)
+            cli.die("Unknown part id %s" % args.part)
             
     @cli.arg('ticket', help='Ticket ID')
     @cli.arg('--delete', action='store_true', help="Delete the ticket")
     @cli.arg('--status', help="Free-form status message")
     @cli.arg('--number', '-n', help='External ticket-tracking number')
     def do_ticket_alter(self, args):
-        q = self.session.query(PartRequest).\
-            filter(PartRequest.id == args.ticket)
+        q = self.session.query(models.PartRequest).\
+            filter(models.PartRequest.id == args.ticket)
         if q.count() != 1:
-            die("Unknown ticket: %s" % args.ticket)
+            cli.die("Unknown ticket: %s" % args.ticket)
         pr = q.first()
         if not pr:
-           die("Unknown ticket with id %s" % (args.ticket))
+           cli.die("Unknown ticket with id %s" % (args.ticket))
         if args.status:
             pr.status = args.status
         if args.number:
@@ -176,22 +101,23 @@ class TicketShell(cli.Shell):
 
     @cli.arg('ticket', help='Ticket ID')
     def do_ticket_close(self, args):
-        pr = self.session.query(PartRequest).filter(
-            PartRequest.id == args.ticket).first()
+        pr = self.session.query(models.PartRequest).filter(
+            models.PartRequest.id == args.ticket).first()
         if not pr:
-           die("Unknown ticket with id %s" % (args.ticket))
+           cli.die("Unknown ticket with id %s" % (args.ticket))
         pr.close()
         self.session.commit()
 
     def do_part_list(self, args):
-        self._table(self.session.query(Part).order_by(Part.id),
+        self._table(self.session.query(models.Part).\
+                                 order_by(models.Part.id),
                     ['id', 'fru', 'description'])
     
     
     @cli.arg('--desc', required=True, help='description of the part') 
     @cli.arg('--fru', help='FRU number')
     def do_part_create(self, args):
-        self._add_obj(Part(description=args.desc, fru=args.fru))
+        self._add_obj(models.Part(description=args.desc, fru=args.fru))
         
 
     @cli.arg('part', help='part id')
@@ -199,26 +125,28 @@ class TicketShell(cli.Shell):
     @cli.arg('--fru', help='FRU number')
     @cli.arg('--delete', action='store_true', help='Delete this entry')
     def do_part_alter(self, args):
-        q = self.session.query(Part).filter(Part.id == args.part)
+        q = self.session.query(models.Part).\
+                         filter(models.Part.id == args.part)
         part = q.first()
         if not part:
-           die("Unknown part with id %s" % (args.part))
+           cli.die("Unknown part with id %s" % (args.part))
         if args.desc:
             part.description = args.desc
         if args.fru:
             part.fru = args.fru
         if args.delete:
-            pr_count = self.session.query(PartRequest).\
-                filter(PartRequest.part_id == args.part).count()
+            pr_count = self.session.query(models.PartRequest).\
+                filter(models.PartRequest.part_id == args.part).count()
             if pr_count != 0:
-                die("Delete failed: Part %s associated with %d tickets!" %
+                cli.die("Delete failed: Part %s associated with %d tickets!" %
                     (args.part, pr_count))
             q.delete()
         self.session.commit()
             
 
     def do_host_list(self, args):
-        self._table(self.session.query(Machine).order_by(Machine.id),
+        self._table(self.session.query(models.Machine).\
+                                 order_by(models.Machine.id),
                     ['id', 'hostname', 'machine_type', 'serial'])
     
     
@@ -226,7 +154,7 @@ class TicketShell(cli.Shell):
     @cli.arg('machine_type', help='Machine model number')
     @cli.arg('serial', help='Serial number of the machine')
     def do_host_create(self, args):
-        self._add_obj(Machine(hostname=args.hostname,
+        self._add_obj(models.Machine(hostname=args.hostname,
             machine_type=args.machine_type, serial=args.serial))
     
     
@@ -235,23 +163,24 @@ class TicketShell(cli.Shell):
     @cli.arg('--serial','-s', help='Serial number of the machine')
     @cli.arg('--delete','-d', action='store_true', help='Delete this entry')
     def do_host_alter(self, args):
-        q = self.session.query(Machine).\
-            filter(Machine.hostname == args.hostname) 
+        q = self.session.query(models.Machine).\
+            filter(models.Machine.hostname == args.hostname) 
         m = q.first()
         if args.type:
             m.machine_type = args.type
         if args.serial:
             m.serial = args.serial
         if args.delete:
-            count = self.session.query(PartRequest).\
-                filter(PartRequest.machine_id == args.id).count()
+            count = self.session.query(models.PartRequest).\
+                filter(models.PartRequest.machine_id == args.id).count()
             if count > 0:
-                die("Delete failed: Machine %s associated with %d tickets!"
+                cli.die("Delete failed: Machine %s associated with %d tickets!"
                     % (args.id, count))
             q.delete()
             self.session.commit()
 
 def main():
+    DEFAULT_DB_PATH = os.path.join(os.environ.get('HOME'), '.owen.sqlite')
     shell = TicketShell(DEFAULT_DB_PATH)
     shell.main(sys.argv[1:])
 
